044d0524db6e26e09e874ab9ef0daed853ce3c99
Mir Parthib Mohammad <mirparthib100@gmail.com>
2026-05-25 23:00:42 +0200

    Initial commit including README

---
+++ src/UdpClient.java
@@ -0,0 +1,136 @@
+import java.io.IOException;
+import java.net.*;
+
+
+
+public class UdpClient {
+
+
+
+    enum state{
+    C0,
+    C1,
+    C2;
+}
+
+
+
+public static void main(String[] args) throws SocketException, UnknownHostException {
+        byte[] buffer = new byte[2];
+        String data = "TheQuickBrownRabbit";
+        //receive teil
+        byte[] receiveBuffer = new byte[2];
+        DatagramPacket receiveSegment = new DatagramPacket(receiveBuffer, 2);
+
+try(DatagramSocket serverSocket = new DatagramSocket(1338)){
+    //timeout
+    serverSocket.setSoTimeout(5000);
+    DatagramPacket sendSegment = new DatagramPacket(buffer, 2, InetAddress.getByName("localhost"), 3887);
+
+   var currentState = state.C0;
+  int  dataPointer = 0;
+  byte KB_0 = 0x00;
+  byte KB_1 = (byte) 0xFF;
+
+    while(true){
+
+        switch (currentState){
+
+            case C0:
+               sendSegment.getData()[0] = (byte) data.charAt(dataPointer);
+                sendSegment.getData()[1] = (byte) KB_0;
+                sendSegment.setPort(3887);
+                serverSocket.send(sendSegment);
+                System.out.println("Sent in C0 (M0): " + (char)sendSegment.getData()[0] + " | KB: " + sendSegment.getData()[1]);
+
+                dataPointer++;
+
+                currentState = state.C1;
+                break;
+            case C1:
+                //sendSegment.setPort(3887); //hardcoding wegen fehler
+                try {
+
+
+                    serverSocket.receive(receiveSegment);
+                    byte receivedL = receiveSegment.getData()[0];
+                    byte receivedC1 = receiveSegment.getData()[1];
+                    System.out.println("Empfangen (A0): '" + (char) receivedL + "' (KB: " + receivedC1 + ")");
+                    //lesen
+
+                    if (receivedC1 == KB_0) {
+                        dataPointer++;
+                        if (dataPointer >= data.length()) {
+                            System.out.println("Finish");
+                            dataPointer = 0;
+                        }
+                        //ändern und senden
+                        sendSegment.getData()[0] = (byte) data.charAt(dataPointer);
+                        sendSegment.getData()[1] = KB_1;
+                        sendSegment.setPort(3887);
+                        serverSocket.send(sendSegment);
+                        System.out.println("Gesendet  (M1): '" + (char) sendSegment.getData()[0] + "' (KB: " + KB_1 + ")");
+
+                        currentState = state.C2;
+
+                    } else {
+
+                        System.out.println("Falsch");
+                    }
+
+                } catch (SocketTimeoutException e){
+                    System.out.println("nochmal schicken wegen timeout");
+                    //nochmal schicken
+                    sendSegment.getData()[0] = (byte) data.charAt(dataPointer);
+                    sendSegment.getData()[1] = (byte) KB_0;
+                    sendSegment.setPort(3887);
+                    serverSocket.send(sendSegment);
+                }break;
+            case C2:
+                try {
+                    serverSocket.receive(receiveSegment);
+                    byte receivedL_C2 = receiveSegment.getData()[0];
+                    byte receivedC2 = receiveSegment.getData()[1];
+                    System.out.println("Empfangen (A1): '" + (char) receivedL_C2 + "' (KB: " + receivedC2 + ")");
+                    if (receivedC2 == KB_1) {
+                        dataPointer++;
+                        if (dataPointer >= data.length()) {
+                            System.out.println("Finish");
+                            dataPointer = 0;
+                        }
+                       //ändern und senden
+                        sendSegment.getData()[0] = (byte) data.charAt(dataPointer);
+                        sendSegment.getData()[1] = KB_0;
+                        sendSegment.setPort(3887);
+                        serverSocket.send(sendSegment);
+                        System.out.println("Gesendet  (M0): '" + (char) sendSegment.getData()[0] + "' (KB: " + KB_0 + ")");
+
+                        currentState = state.C1;
+
+                    } else {
+
+                        System.out.println("Falsch");
+                    }
+                }catch (SocketTimeoutException e){
+                    System.out.println("nochmal schicken wegen timeout");
+                    //nochmal schicken
+                    sendSegment.getData()[0] = (byte) data.charAt(dataPointer);
+                    sendSegment.getData()[1] = KB_1;
+                    sendSegment.setPort(3887);
+                    serverSocket.send(sendSegment);
+
+                }
+                break;
+
+
+        }
+    }
+
+
+} catch (IOException e) {
+    throw new RuntimeException(e);
+}
+
+
+}
+}
---
+++ src/UdpServer.java
@@ -0,0 +1,95 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UdpServer {
+
+
+
+
+    enum state{
+        S1, //warte auf A0 und sendet M0 mit KB 0
+        S2; //mit KB 1
+    }
+
+
+    public static void main(String[] args) {
+        String data = "JumpsOverTheLazyFox";
+        int dataPointer = 0;
+        byte KB_0 = 0x00;
+        byte KB_01 = (byte) 0xFF;
+        final byte[] buffer = new byte[2];
+
+        state currentState = state.S1; //startet mit s1
+
+        byte[] receivedBuffer = new byte[2];
+        DatagramPacket receivedSegment = new DatagramPacket(receivedBuffer, 2);
+
+        try (DatagramSocket serverSocket = new DatagramSocket(1337)) {     // client socket 1338 server 1337
+                DatagramPacket sendSegment = new DatagramPacket(buffer, 2, InetAddress.getByName("localhost"), 3888); // packet
+            System.out.println("Server waiting...");
+                while (true) {
+
+                    serverSocket.receive(receivedSegment); // block wartet auf Nachricht
+
+                    //lesen
+                    byte receivedL = receivedSegment.getData()[0];
+                    byte receivedKB = receivedSegment.getData()[1];
+
+                    System.out.println("Text: " + (char)receivedL + " Kontrollbyte: " + receivedKB);
+
+                switch (currentState){
+
+                    case S1:
+
+                        if(receivedKB == KB_0){
+                                if (dataPointer >= data.length()){
+                                    System.out.println("Finish");
+                                    dataPointer = 0;
+                                }
+                            sendSegment.getData()[0] = (byte) data.charAt(dataPointer); //hole Buchstaben ab
+                            sendSegment.getData()[1] = KB_0; // schreibe 0x00 in getData 1
+                            sendSegment.setPort(3888); // hardgecoded weil error gab
+                            serverSocket.send(sendSegment); //sende M0
+                            System.out.println("Empfangen (A0): '" + (char)receivedL + "' (KB: " + receivedKB + ")");
+                            System.out.println("Gesendet  (M0): '" + (char)sendSegment.getData()[0] + "' (KB: " + KB_0 + ")");
+                            dataPointer++;
+                            currentState = state.S2; // jetzt zum s2 wechseln
+                        }
+                        else {
+                            System.out.println("Fehler Falsches Kontrollbyte");
+                        }
+                        break;
+                    case S2:
+                        if(receivedKB == KB_01){
+                            if (dataPointer >= data.length()) {
+                                dataPointer = 0;
+                            }
+                            sendSegment.getData()[0] = (byte) data.charAt(dataPointer);
+                            sendSegment.getData()[1] = KB_01; // schreibe 0xFF in getData 1
+                            sendSegment.setPort(3888);
+                            serverSocket.send(sendSegment); //sende M1
+                            System.out.println("Empfangen (A1): '" + (char)receivedL + "' (KB: " + receivedKB + ")");
+                            System.out.println("Gesendet  (M1): '" + (char)sendSegment.getData()[0] + "' (KB: " + KB_01 + ")");
+                            dataPointer++;
+                            currentState = state.S1;
+                        }
+                        else {
+                            System.out.println("Fehler Falsches Kontrollbyte");
+                        }
+                        break;
+
+
+
+
+                }
+
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+
+
+    }
+}
